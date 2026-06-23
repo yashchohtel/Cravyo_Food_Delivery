8a28ca5ac40e9f77d53c05f8f92f6b06b8b13840
Yash Chohtel <yashchohtel@gmail.com>
2026-06-23 23:30:01 +0530

    user modal building started

---
+++ Backend/models/userModel.js
@@ -0,0 +1,3 @@
+import mongoose from "mongoose"; // Import mongoose
+import bcrypt from "bcryptjs"; // Import bcrypt for password hashing
+import jwt from "jsonwebtoken"; // Import JWT for authentication
