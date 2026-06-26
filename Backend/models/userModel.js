e86c68528b2c6a162c57205ae8372d5a69363bab
Yash Chohtel <yashchohtel@gmail.com>
2026-06-26 12:29:43 +0530

    folder structure redefined

--- Backend/models/userModel.js
+++
@@ -1,3 +0,0 @@
-import mongoose from "mongoose"; // Import mongoose
-import bcrypt from "bcryptjs"; // Import bcrypt for password hashing
-import jwt from "jsonwebtoken"; // Import JWT for authentication
