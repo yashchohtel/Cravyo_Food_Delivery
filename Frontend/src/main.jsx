943efdae6a0d67e8f38b5abc5957a25299c28dcb
Yash Chohtel <yashchohtel@gmail.com>
2026-07-04 23:44:03 +0530

    authentication calling started

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './app/store.js'
+import store from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
 
