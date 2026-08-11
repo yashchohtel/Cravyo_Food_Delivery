37987589db38cfff1ea5e0b24ec043c6c0e893c8
Yash Chohtel <yashchohtel@gmail.com>
2026-08-11 23:34:04 +0530

    map pin icon building started

--- Frontend/src/Components/Ui/Map/Map.jsx
+++ Frontend/src/Components/Ui/Map/Map.jsx
@@ -65,7 +65,7 @@ const Map = () => {
                 )}
 
                 <div className="pinContainer">
-                    <FaLocationDot/>
+                    <FaLocationDot className="locationPin"/>
                 </div>
 
             </MapContainer>
