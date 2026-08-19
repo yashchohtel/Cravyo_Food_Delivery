6acfcdfa8cc813894f667c29269b279ec751ae85
Yash Chohtel <yashchohtel@gmail.com>
2026-08-19 21:38:04 +0530

    slider ui done

--- Frontend/src/Components/Sliders/Home Promotion Slider/HomePromotionSlider.jsx
+++ Frontend/src/Components/Sliders/Home Promotion Slider/HomePromotionSlider.jsx
@@ -1,29 +1,62 @@
 import './HomePromotionSlider.css'
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Autoplay, Pagination } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/pagination";
 const HomePromotionSlider = () => {
+
+    const images = [
+        "/heroBanner (1).png",
+        "/heroBanner (2).png",
+        "/heroBanner (3).png",
+        "/heroBanner (4).png",
+        "/heroBanner (5).png",
+    ];
 
     return (
 
         <>
 
             {/* slide container */}
-            <div className="slideContainer container">
+            <div className="promotionSlideCont container">
 
-                <Swiper>
-                    <SwiperSlide>Banner 1</SwiperSlide>
-                    <SwiperSlide>Banner 2</SwiperSlide>
-                    <SwiperSlide>Banner 3</SwiperSlide>
-                </Swiper>
+                <section className="promotion-slider-wrapper">
+
+                    <Swiper
+                        modules={[Autoplay, Pagination]}
+                        slidesPerView={1}
+                        spaceBetween={16}
+                        loop={true}
+                        autoplay={{
+                            delay: 4000,
+                            disableOnInteraction: false,
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        className="promotion-slider"
+                    >
+                        {images.map((image, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="promotion-slide">
+                                    <img
+                                        src={image}
+                                        alt={`Promotion ${index + 1}`}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+
+                </section>
 
             </div>
+
         </>
 
     )
 
 }
-
-
 
 export default HomePromotionSlider;
