c02fff986ccbd176c3be9bd760a00051285dbc35
Yash Chohtel <yashchohtel@gmail.com>
2026-08-24 23:58:04 +0530

    admin side bar completed

--- Frontend/src/Components/Sidebars/Admin Sidebar/AdminSidebar.jsx
+++ Frontend/src/Components/Sidebars/Admin Sidebar/AdminSidebar.jsx
@@ -1,24 +1,85 @@
+import { FiCoffee, FiGrid, FiImage } from 'react-icons/fi';
 import './AdminSidebar.css'
 import { NavLink } from "react-router-dom";
+import { RiMenu2Line } from "react-icons/ri";
+import { IoCloseSharp } from "react-icons/io5";
+import { useState } from 'react';
+
 
 const AdminSidebar = () => {
+
+    // state to store sidevar show hide status
+    const [showSidebar, setShowSidebar] = useState(true);
+
+    // function to toggle sidebar
+    const toggleSidebar = () => {
+        setShowSidebar((prev) => !prev);
+    };
 
     return (
 
         <>
             <aside className="admin-sidebar">
 
-                <NavLink to="/admin" end>
-                    Dashboard
-                </NavLink>
+                {/* sidebar toggle button */}
+                <div
+                    className="sidebarToggle"
+                    onClick={toggleSidebar}
+                >
 
-                <NavLink to="/admin/banners">
-                    Banners
-                </NavLink>
+                    {showSidebar ? (
+                        <div className="close toggleIcon">
+                            <IoCloseSharp />
+                        </div>
+                    ) : (
+                        <div className="open toggleIcon">
+                            <RiMenu2Line />
+                        </div>
+                    )}
 
-                <NavLink to="/admin/food-categories">
-                    Food Categories
-                </NavLink>
+                </div>
+
+                <nav className="sidebar-nav">
+
+                    <NavLink
+                        to="/admin"
+                        end
+                        className={`sidebar-link ${showSidebar ? "" : "activeLink"}`}
+                    >
+                        <FiGrid className="sidebar-icon" />
+
+                        <span className={`sidebar-text ${showSidebar ? "" : "activeText"}`}>
+                            Dashboard
+                        </span>
+
+                    </NavLink>
+
+
+                    <NavLink
+                        to="/admin/banners"
+                        className={`sidebar-link ${showSidebar ? "" : "activeLink"}`}
+                    >
+                        <FiImage className="sidebar-icon" />
+
+                        <span className={`sidebar-text ${showSidebar ? "" : "activeText"}`}>
+                            Banners
+                        </span>
+
+                    </NavLink>
+
+                    <NavLink
+                        to="/admin/food-categories"
+                        className={`sidebar-link ${showSidebar ? "" : "activeLink"}`}
+                    >
+                        <FiCoffee className="sidebar-icon" />
+
+                        <span className={`sidebar-text ${showSidebar ? "" : "activeText"}`}>
+                            Food Categories
+                        </span>
+
+                    </NavLink>
+
+                </nav>
 
             </aside>
         </>
